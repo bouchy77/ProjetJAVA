2675a4c5ab7db76cedcac8fb3bb70c05aadd65a5
root <root@localhost.localdomain>
2019-12-06 07:41:48 -0800

    Tant que je n'ai pas fini...

--- src/oata/Test.java
+++ src/oata/Test.java
@@ -1,6 +1,6 @@
 public class Test{
 public static void main(String[] args){
-	System.out.println("----------------");
+	System.out.println("---------------J'affiche cela-----------------");
 	System.out.println("Pipe DevOps : monCode source, git, build avec Jenkins");
 }
 }
