1ba872ba3f737cfeeba2502eb62d6bd3f5fca548
bouchy77 <bouchy77@gmail.com>
2019-12-08 17:47:03 +0100

    Fonctionnalites bouchra

--- src/oata/Test.java
+++ src/oata/Test.java
@@ -2,5 +2,7 @@ public class Test{
 public static void main(String[] args){
 	System.out.println("---------------J'affiche cela-----------------");
 	System.out.println("Pipe DevOps : monCode source, git, build avec Jenkins");
+	System.out.println("Dev de Bouchra, fonction sommer");
+
 }
 }
