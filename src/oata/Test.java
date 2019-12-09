af16c572e2bc91eb7f85012dc7feb7cc0f9c1ade
Bruno Zeitouni <bzeitouni@gmail.com>
2019-12-09 11:13:08 +0100

    modif1 bruno

--- src/oata/Test.java
+++ src/oata/Test.java
@@ -3,6 +3,6 @@ public static void main(String[] args){
 	System.out.println("---------------J'affiche cela-----------------");
 	System.out.println("Pipe DevOps : monCode source, git, build avec Jenkins");
 	System.out.println("Dev de Bouchra, fonction sommer");
-
+	System.out.printeln("Dev de Bruno, fonction test1");
 }
 }
