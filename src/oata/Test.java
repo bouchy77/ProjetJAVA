a4f55ef469f4f7580a174737986b5d887e90e6c8
Dangeard <enilyam@hotmail.fr>
2019-12-09 11:03:58 +0000

    correction erreur point virgule

--- src/oata/Test.java
+++ src/oata/Test.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 	System.out.println("Pipe DevOps : monCode source, git, build avec Jenkins");
 	System.out.println("Dev de Bouchra, fonction sommer");
 	System.out.printeln("Dev de Bruno, fonction test1");
-	System.out.printeln("Dev de Bruno, fonction test2")
+	System.out.printeln("Dev de Bruno, fonction test2");
 
 }
 }
